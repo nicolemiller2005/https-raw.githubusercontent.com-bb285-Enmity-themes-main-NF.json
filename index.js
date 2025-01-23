fa1d014c44983f944590a80141f3890a79ccb852
bigcodepro <63102701+nicolemiller2005@users.noreply.github.com>
2025-01-23 02:26:01 +0000

    index.js

--- index.js
+++ index.js
@@ -1,38 +1,62 @@
 {
-    ":root": {
-        "backgroundImage": "url('https://imageio.forbes.com/specials-images/imageserve/1165871313/2019-Lollapalooza---Day-2/960x0.jpg')",
-        "backgroundImageBlur": "3px",
-        "backgroundImageSize": "cover",
-        "backgroundImagePosition": "center",
-        "popoutImage": "var(--background-image)",
-        "popoutImageBlur": "1px",
-        "popoutImageSize": "cover",
-        "popoutImagePosition": "center",
-        "overlayImage": "var(--background-image)",
-        "overlayImageBlur": "5px",
-        "overlayImageSize": "cover",
-        "overlayImagePosition": "center",
-        "homeButtonImage": "url('https://imageio.forbes.com/specials-images/imageserve/1165871313/2019-Lollapalooza---Day-2/960x0.jpg')",
-        "homeButtonSize": "cover",
-        "homeButtonPosition": "center",
-        "serverlistBrightness": 0.56,
-        "leftBrightness": 0.52,
-        "middleBrightness": 0.64,
-        "rightBrightness": 0,
-        "overlayBrightness": 0.54,
+    "theme": {
+        "name": "Custom Lollapalooza Theme",
+        "description": "A custom theme inspired by the Lollapalooza image",
+        "author": "YourName",
+        "version": "1.0.0"
+    },
+    "background": {
+        "image": "https://imageio.forbes.com/specials-images/imageserve/1165871313/2019-Lollapalooza---Day-2/960x0.jpg",
+        "blur": "3px",
+        "size": "cover",
+        "position": "center"
+    },
+    "popout": {
+        "image": "https://imageio.forbes.com/specials-images/imageserve/1165871313/2019-Lollapalooza---Day-2/960x0.jpg",
+        "blur": "1px",
+        "size": "cover",
+        "position": "center"
+    },
+    "overlay": {
+        "image": "https://imageio.forbes.com/specials-images/imageserve/1165871313/2019-Lollapalooza---Day-2/960x0.jpg",
+        "blur": "5px",
+        "size": "cover",
+        "position": "center",
+        "brightness": 0.54
+    },
+    "buttons": {
+        "home": {
+            "image": "https://imageio.forbes.com/specials-images/imageserve/1165871313/2019-Lollapalooza---Day-2/960x0.jpg",
+            "size": "cover",
+            "position": "center"
+        }
+    },
+    "brightness": {
+        "serverlist": 0.56,
+        "left": 0.52,
+        "middle": 0.64,
+        "right": 0
+    },
+    "colors": {
         "gradientPrimary": [240, 17, 183],
         "gradientSecondary": [0, 0, 0],
-        "linkColour": "#EC00E2",
-        "scrollbarColour": ["rgb(7,7,7,0)"],
-        "gradientDirection": '192deg',
-        'tintColour': [255, 51, 159],
-		'tintBrightness': 0,
-		'font': 0,
-		'windowPadding': '10px',
-		'windowRoundness': '21px',
-		'updateNotice1': 'none',
-		'columns': 2,
-		'guildgap': '3',
-		'aligndms': '1'
+        "link": "#EC00E2",
+        "scrollbar": "rgba(7, 7, 7, 0)"
+    },
+    "effects": {
+        "gradientDirection": "192deg",
+        "tintColor": [255, 51, 159],
+        "tintBrightness": 0
+    },
+    "font": 0,
+    "window": {
+        "padding": "10px",
+        "roundness": "21px"
+    },
+    "updateNotice": "none",
+    "layout": {
+        "columns": 2,
+        "guildGap": "3",
+        "alignDMs": true
     }
 }
